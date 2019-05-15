db0342dafeba0e9ad67677ee39dd87e4a235a979
Daniel Seehausen <daniel.seehausen@gmail.com>
2019-05-15 10:12:14 -0400

    this is some super old stuff i never thought I would use but here I am, at a wework hackathon, about to use it

--- api_interface/tracks.py
+++ api_interface/tracks.py
@@ -2,6 +2,33 @@
 from .helpers import *
 print('Running' if __name__ == '__main__' else 'Importing', Path(__file__).resolve())
 
+
+def get_track_canonical_urls(track_id):
+    track_dict = get("track", track_id)
+
+    urls = []
+
+    print("Fetching...\n\n")
+    print(track_dict["title"])
+
+    for topic in track_dict["children"]:
+        print("\n\t", topic["title"])
+        for unit in topic["children"]:
+            print("\t\t", unit["title"])
+            for lesson in unit["children"]:
+                full_lesson_data = get("lesson", lesson["id"])
+                print("\t\t\t - ", lesson["title"], end="...")
+                try:
+                    url = get_url(full_lesson_data)
+                    urls.append(url)
+                except:
+                    print("<---- BAD DATA HERE. LESSON OBJECT FOLLOWED BY ERROR:")
+                    print(lesson)
+                    raise
+                print("success")
+
+    return (urls)
+
 def get_track(track_id):
     track_dict = get("track", track_id)
 
@@ -22,6 +49,7 @@ def get_track(track_id):
                 full_lesson_data = get("lesson", lesson["id"])
                 print("\t\t\t - ", lesson["title"], end="...")
                 try:
+                    get_url(full_lesson_data)
                     found_lessons.add(full_lesson_data["canonical_id"])
                     parent_data = [track_title, topic_title, unit_title, len(rows)]
                     lesson_data = parse_lesson_to_row(full_lesson_data)
--- tester.py
+++ tester.py
@@ -3,27 +3,13 @@
 from api_interface import *
 
 def main():
-    mod4_rows, mod4_lessons = get_track(25783)
-    redux_rows, redux_lessons = get_track(23286)
-    in_person_rows, in_person_ids = get_online_redux_react()
-    all_rows = mod4_rows + redux_rows + in_person_rows
-
-    # in_person_ids = mod4_lessons.union(redux_lessons)
-    # unique_online_rows, online_ids = temper(in_person_ids)
-    # overlap_ids = in_person_ids.intersection(online_ids)
-    # all_rows = mod4_rows + redux_rows + unique_online_rows
-    #
-    # for row in all_rows:
-    #     if row[4] in overlap_ids:
-    #         row.insert(0, 'B')
-    #     elif row[4] in in_person_ids:
-    #         row.insert(0, 'I')
-    #     else:
-    #         row.insert(0, 'O')
-
-    HEADERS = ["Track", "Topic", "Unit", "Position", "CID", "Title", "CName", "Content Type", "Github", "ReviewStatus", "Action", "Merge Status", "Owner", "Reviewer A", "Reviewer B"]
-    header_and_rows = [HEADERS] + all_rows
-    to_csv(all_rows)
+    l1_lectures = get_track_canonical_urls(40234)
+    l1_student_lessons = get_track_canonical_urls(40864)
+    l1_teacher_training = get_track_canonical_urls(34621)
+    print(l1_lectures, l1_student_lessons, l1_teacher_training)
+    # HEADERS = ["Track", "Topic", "Unit", "Position", "CID", "Title", "CName", "Content Type", "Github", "ReviewStatus", "Action", "Merge Status", "Owner", "Reviewer A", "Reviewer B"]
+    # header_and_rows = [HEADERS] + all_rows
+    # to_csv(all_rows)
 
 if __name__ == '__main__':
     main()
